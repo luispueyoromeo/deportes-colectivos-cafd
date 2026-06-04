40d89b8f693c1a512fa2488fe4943d2482eb5a8d
luispueyoromeo <luispueyoromeo@gmail.com>
2026-06-04 12:20:24 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -38,7 +38,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre el reglamento de voleibol durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1jsfS0zu9059mpTXnPJTDwJcUVqHhEAPs?usp=sharing',
   },
   {
     id: 'voleibol-reglamento-2026-2027',
@@ -48,7 +48,7 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre el reglamento de voleibol durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1Mb7U1sxO9MhgJzXgohF2KR6su8iVao5d?usp=sharing',
   },
   {
     id: 'voleibol-fundamentos-tecnicos-2025-2026',
@@ -58,7 +58,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos técnicos de voleibol durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1mYO26vCTo8oAW7q-YV-hYegLZBA90xA9?usp=sharing',
   },
   {
     id: 'voleibol-fundamentos-tecnicos-2026-2027',
@@ -68,7 +68,7 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos técnicos de voleibol durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/14yidUwUp4RZjJH-npP8KVujxEfeM-EdZ?usp=sharing',
   },
   {
     id: 'voleibol-fundamentos-tacticos-2025-2026',
@@ -78,7 +78,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos tácticos de voleibol durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1fcu-BLo8zG5EIFDnoEqFkNsD-oQfAkbm?usp=sharing',
   },
   {
     id: 'voleibol-fundamentos-tacticos-2026-2027',
@@ -88,7 +88,7 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos tácticos de voleibol durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/17PHTgqbvDN_qrxS0GeuXXjj8SEuiAL5j?usp=sharing',
   },
   {
     id: 'baloncesto-reglamento-2025-2026',
@@ -98,7 +98,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre el reglamento de baloncesto durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1BpPpRMj3gHItqEEQbNHQyLFZjktm10AJ?usp=sharing',
   },
   {
     id: 'baloncesto-reglamento-2026-2027',
@@ -108,7 +108,7 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre el reglamento de baloncesto durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1lqRNfDQT0euNRO6driNSReTGh9E3zl9R?usp=sharing',
   },
   {
     id: 'baloncesto-fundamentos-tecnicos-2025-2026',
@@ -118,7 +118,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos técnicos de baloncesto durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1UPLAaYSUuI44prv3UX6Vw0J14Jac6ERL?usp=sharing',
   },
   {
     id: 'baloncesto-fundamentos-tecnicos-2026-2027',
@@ -128,7 +128,7 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos técnicos de baloncesto durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1O_bIdoX3GatWXLKcKLtlFCDLXVkR8zzg?usp=sharing',
   },
   {
     id: 'baloncesto-fundamentos-tacticos-2025-2026',
@@ -138,7 +138,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos tácticos de baloncesto durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1AjfUcJAxiOVkAQx0JKLnfqUg8mnAQddb?usp=sharing',
   },
   {
     id: 'baloncesto-fundamentos-tacticos-2026-2027',
@@ -148,7 +148,7 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos tácticos de baloncesto durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/13E-dMQKllWFyC9Ejoy_yxRhh0pAA6YNr?usp=sharing',
   },
   {
     id: 'balonmano-reglamento-2025-2026',
@@ -158,7 +158,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre el reglamento de balonmano durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/15Tl0Dd3hb3xhrwyOV8kgT0ISWeYEXKHu?usp=sharing',
   },
   {
     id: 'balonmano-reglamento-2026-2027',
@@ -168,7 +168,7 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre el reglamento de balonmano durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/15xjMc13mFKERSGDsQAd3oBGCmchcdinm?usp=sharing',
   },
   {
     id: 'balonmano-fundamentos-tecnicos-2025-2026',
@@ -178,7 +178,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos técnicos de balonmano durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1iwevcgz5pWZnTlfXYmESI2B1b_9gigkr?usp=sharing',
   },
   {
     id: 'balonmano-fundamentos-tecnicos-2026-2027',
@@ -188,7 +188,7 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos técnicos de balonmano durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1uRKraofwWXFTmYpLgwplOedfd-C0Pzir?usp=sharing',
   },
   {
     id: 'balonmano-fundamentos-tacticos-2025-2026',
@@ -198,7 +198,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos tácticos de balonmano durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/12YLDRe3Ttd_uWU_7BDU48TSrioj-kP0E?usp=sharing',
   },
   {
     id: 'balonmano-fundamentos-tacticos-2026-2027',
@@ -208,7 +208,7 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos tácticos de balonmano durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1M76UY6IW2Z7OSB3iMEo01AQDxDL3RQWE?usp=sharing',
   },
   {
     id: 'futbol-reglamento-2025-2026',
@@ -218,7 +218,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre el reglamento de fútbol durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/12jPK_Ukk4i0NFxvgoq47NVkNUeqdhL6P?usp=sharing',
   },
   {
     id: 'futbol-reglamento-2026-2027',
@@ -228,7 +228,7 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre el reglamento de fútbol durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1hDwIZau6tbVQ0yI7l0yoOiYF6kuEy1bf?usp=sharing',
   },
   {
     id: 'futbol-fundamentos-tecnicos-2025-2026',
@@ -238,7 +238,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos técnicos de fútbol durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1RmVPJcu488aPRbOuwwaEEGgnZDqLjlwB?usp=sharing',
   },
   {
     id: 'futbol-fundamentos-tecnicos-2026-2027',
@@ -248,7 +248,7 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos técnicos de fútbol durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1Vii1n_jW__oTQbGG7FPkyy05xu4j7QYL?usp=sharing',
   },
   {
     id: 'futbol-fundamentos-tacticos-2025-2026',
@@ -258,7 +258,7 @@ export const resources = [
     academicYear: '2025/2026',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos tácticos de fútbol durante el curso académico 2025/2026.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/14aAuq44JZy3frJUQFtAT9u8b5NW3FKmZ?usp=sharing',
   },
   {
     id: 'futbol-fundamentos-tacticos-2026-2027',
@@ -268,6 +268,6 @@ export const resources = [
     academicYear: '2026/2027',
     description:
       'Carpeta recopilatoria con materiales de aula invertida elaborados por el alumnado sobre fundamentos tácticos de fútbol durante el curso académico 2026/2027.',
-    driveUrl: 'https://drive.google.com/drive/folders/ENLACE_EDITABLE',
+    driveUrl: 'https://drive.google.com/drive/folders/1hVxUqfljR0ppLO6BmAeIZa0y4sPQUbWq?usp=sharing',
   },
 ];
